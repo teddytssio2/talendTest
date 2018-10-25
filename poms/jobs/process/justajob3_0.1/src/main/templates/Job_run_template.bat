60b64110b3457cd12ae745cd4041b51f80265c58
Teddy LEQUETTE <Teddy.LEQUETTE@hardis.fr>
2018-10-25 11:36:53 +0200

    first commit

---
+++ poms/jobs/process/justajob3_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/antlr-runtime-3.5.2.jar;../lib/dom4j-1.6.1.jar;../lib/junit.jar;../lib/log4j-1.2.16.jar;../lib/org.talend.dataquality.parser.jar;../lib/talend_file_enhanced_20070724.jar;../lib/talendcsv.jar;justajob3_0_1.jar; projet3.justajob3_0_1.JustAJob3  %* 
