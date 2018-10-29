745d5c7d3aec7215f657d5e8e0522ac378171d6a
Teddy LEQUETTE <Teddy.LEQUETTE@hardis.fr>
2018-10-29 14:45:58 +0100

    Mon deuxieme commit

--- poms/jobs/process/justajob3_0.1/src/main/templates/Job_run_template.ps1
+++ poms/jobs/process/justajob3_0.1/src/main/templates/Job_run_template.ps1
@@ -1,3 +1,3 @@
 $fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 cd $fileDir
-java '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/antlr-runtime-3.5.2.jar;../lib/dom4j-1.6.1.jar;../lib/junit.jar;../lib/log4j-1.2.16.jar;../lib/org.talend.dataquality.parser.jar;../lib/talend_file_enhanced_20070724.jar;../lib/talendcsv.jar;justajob3_0_1.jar;' projet3.justajob3_0_1.JustAJob3  %* 
+java '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/antlr-runtime-3.5.2.jar;../lib/dom4j-1.6.1.jar;../lib/junit.jar;../lib/log4j-1.2.16.jar;../lib/org.talend.dataquality.parser.jar;../lib/talend_file_enhanced_20070724.jar;../lib/talendcsv.jar;justajob3_0_1.jar;' projet3.justajob3_0_1.JustAJob3  --context=Default %* 
--- poms/jobs/process/justajob3_0.1/src/main/templates/Job_run_template.sh
+++ poms/jobs/process/justajob3_0.1/src/main/templates/Job_run_template.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 ROOT_PATH=`pwd`
-java -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/antlr-runtime-3.5.2.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/junit.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/../lib/org.talend.dataquality.parser.jar:$ROOT_PATH/../lib/talend_file_enhanced_20070724.jar:$ROOT_PATH/../lib/talendcsv.jar:$ROOT_PATH/justajob3_0_1.jar: projet3.justajob3_0_1.JustAJob3  "$@" 
+java -Xms256M -Xmx1024M -cp .:$ROOT_PATH:$ROOT_PATH/../lib/routines.jar:$ROOT_PATH/../lib/antlr-runtime-3.5.2.jar:$ROOT_PATH/../lib/dom4j-1.6.1.jar:$ROOT_PATH/../lib/junit.jar:$ROOT_PATH/../lib/log4j-1.2.16.jar:$ROOT_PATH/../lib/org.talend.dataquality.parser.jar:$ROOT_PATH/../lib/talend_file_enhanced_20070724.jar:$ROOT_PATH/../lib/talendcsv.jar:$ROOT_PATH/justajob3_0_1.jar: projet3.justajob3_0_1.JustAJob3  --context=Default "$@" 
